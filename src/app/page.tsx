11762802df9269765223586d069c4c7f8aacc12b
tengkuhafidz <tengkuhafidz@gmail.com>
2024-01-18 00:26:41 +0800

    enhance social image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { FaChildren, FaUserDoctor } from "react-icons/fa6";
 import { GiAmbulance } from "react-icons/gi";
 import { IoWoman } from "react-icons/io5";
 import { MdRecordVoiceOver } from "react-icons/md";
-import shortNumber from 'short-number';
+import abbreviate from 'number-abbreviate';
 import Headline from "./Headline";
 import Stat from "./Stat";
 
@@ -22,15 +22,20 @@ export async function generateMetadata(
   const { martyred } = await getData()
   const daysOfWarCrimes = dayjs().diff('2023-10-07', 'day')
 
+  const title = 'Gaza in Numbers 🇵🇸'
+  const description = 'Stay informed on the latest Gaza-Nakba numbers'
+  const ogImgTitle = `${daysOfWarCrimes} Days. ${abbreviate(martyred.total)}+ Killed`
+  const ogImgFooter = '#CeasefireNOW'
+
   return {
-    title: 'Gaza in Numbers 🇵🇸',
-    description: 'Stay informed on the latest Gaza-Nakba numbers',
-    metadataBase: new URL('https://gaza-numbers.jariyah.app'),
+    title,
+    description,
+    metadataBase: new URL('https://gazanumbers.jariyah.app'),
     openGraph: {
-      title: 'Gaza in Numbers 🇵🇸',
-      description: 'Stay informed on the latest Gaza-Nakba numbers',
-      siteName: 'Gaza in Numbers 🇵🇸',
-      images: `https://og.tailgraph.com/og?fontFamily=Roboto&title=${daysOfWarCrimes}%20Days.%20${shortNumber(martyred.total)}+%20Killed&titleTailwind=font-bold%20text-6xl%20text-white&text=Stay%20informed%20on%20the%20latest%20Gaza-Nakba%20martyrs%20numbers&textTailwind=text-2xl%20mt-4%20text-white&logoTailwind=h-8&bgTailwind=bg-black&footer=%23CeasefireNOW&footerTailwind=text-white&t=1705504316657&refresh=1`,
+      title,
+      description: description,
+      siteName: title,
+      images: `https://og.tailgraph.com/og?fontFamily=Roboto&title=${encodeURI(ogImgTitle)}&titleTailwind=font-bold%20text-6xl%20text-white&text=${encodeURI(description)}&textTailwind=text-2xl%20mt-4%20text-white&logoTailwind=h-8&bgTailwind=bg-black&footer=${ogImgFooter}&footerTailwind=text-white&t=1705504316657&refresh=1`,
     }
   }
 }
